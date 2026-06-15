3ace8581f240bd58814bd43ac97d95f180b6f301
Dmitry Morozov <9984398@gmail.com>
2026-06-15 11:48:22 +0300

    fix(server): импорт stdSerializers из pino отдельным экспортом

--- packages/server/src/logger.ts
+++ packages/server/src/logger.ts
@@ -1,8 +1,8 @@
-import { pino } from "pino";
+import { pino, stdSerializers } from "pino";
 
 export const log = pino({
   level: process.env.LOG_LEVEL ?? "info",
-  serializers: { err: pino.stdSerializers.err },
+  serializers: { err: stdSerializers.err },
 });
 
 export type Logger = typeof log;
